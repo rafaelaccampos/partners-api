0bbf31dd3ab08ba25b7d8d69c174c8ee5a871e43
Rafaela Cassiano Campos <rafaela-cassiano@hotmail.com>
2024-06-17 21:39:36 -0300

    feat: Created events crud template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EventsController } from './events/events.controller';
+import { EventsModule } from './events/events.module';
+import { EventsService } from './events/events.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [EventsModule],
+  controllers: [AppController, EventsController],
+  providers: [AppService, EventsService],
 })
 export class AppModule {}
